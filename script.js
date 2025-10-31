644179226d8b494cb1736ccfa98a67a183af5a57
VINAY-YALAMANCHI <vyalamanchi@rivier.edu>
2025-10-31 12:08:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Dynamic year in footer
+document.getElementById("year").textContent = new Date().getFullYear();
+
+function downloadResume() {
+  alert("👉 Replace this with your resume download link!");
+}
